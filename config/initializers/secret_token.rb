9203599a3562f19de1eaf9665a3d1f757e56c67d
Crystal08 <crystal08@compuserve.com>
2012-11-27 10:59:26 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneTeam::Application.config.secret_token = 'd45b7f5ff3d9ad71e7e3b66329b3a7e7748df580ddb548ae4611c3bc54b00eefa8e120d46982a72e161447d2536d93cdbb2820fe72eb1cda6cc08c71049ad842'
