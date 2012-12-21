deab879c60996d0d64e8fc5a3ac96de96030ca1e
Crystal08 <crystal08@compuserve.com>
2012-12-21 13:27:02 -0600

    Employees have passwords
    Signed-off-by: Crystal08 <crystal08@compuserve.com>

--- db/migrate/20121203214825_add_requester_id_to_requests.rb
+++
@@ -1,4 +0,0 @@
-class AddRequesterIdToRequests < ActiveRecord::Migration
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121205160601) do
+ActiveRecord::Schema.define(:version => 20121221165129) do
 
   create_table "employees", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20121205160601) do
     t.string   "skills_interested_in"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
+    t.string   "password_digest"
   end
 
   create_table "requests", :force => true do |t|
