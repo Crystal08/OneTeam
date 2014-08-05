96d09d6bb481f60ebe61b67be734e15c439f6c72
Crystal08 <Crystal08@compuserve.com>
2014-08-05 13:06:28 -0500

    Fixes minor issues found while testing prior to Rails 4 migration

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,10 +1,11 @@
 class Selection < ActiveRecord::Base
   attr_accessible :notes, :response_id, :created_at
   #Took out :request_id b/c request can be accessed through response
+  #No :employee_id b/c request can be accessed through response
 
   belongs_to :response
-  belongs_to :request
-  belongs_to :employee
+  #belongs_to :request
+  #belongs_to :employee
 
   has_many :responses
   has_many :requests, :through => :responses  
